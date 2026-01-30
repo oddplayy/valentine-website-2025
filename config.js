ab80d1c58c86e912d1c640923e4f96b39f8bd0f6
oddplayy <oddplayy@gmail.com>
2026-01-29 22:22:25 -0700

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://youtu.be/RG9xHEF1vyk?si=xgGecGtaEwoGzeQG", // Music streaming URL
+        musicUrl: "https://youtu.be/7e1uISww4dA?si=ZfKuBp3wrVBFYGjm", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
